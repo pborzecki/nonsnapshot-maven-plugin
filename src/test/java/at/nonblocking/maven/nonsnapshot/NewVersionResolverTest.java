686270a8e901ba9b83a89ea87818c49a75b96b6b
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2016-04-27 20:18:10 +0300

    create NewVersionResolverTest

---
+++ src/test/java/at/nonblocking/maven/nonsnapshot/NewVersionResolverTest.java
@@ -0,0 +1,32 @@
+package at.nonblocking.maven.nonsnapshot;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static at.nonblocking.maven.nonsnapshot.NonSnapshotBaseMojo.DEFAULT_INCREMENT_VERSION_PATTERN;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Yablokov Aleksey
+ */
+public class NewVersionResolverTest {
+    private static final NewVersionResolver resolver = new NewVersionResolver(true, DEFAULT_INCREMENT_VERSION_PATTERN, null);
+
+    @Test
+    public void noAppendBranchNameToVersion() {
+        NewVersionResolver resolver = new NewVersionResolver(false, DEFAULT_INCREMENT_VERSION_PATTERN, null);
+        assertThat(resolver.resolveNewVersion("1.2.3", "master"), equalTo("1.2.4"));
+    }
+
+    @Test
+    public void appendBranchNameToVersion() {
+        assertThat(resolver.resolveNewVersion("1.2.3", "master"), equalTo("1.2.3-master-1"));
+    }
+
+    @Test
+    @Ignore("need fix")
+    public void replaceSuffix() {
+        assertThat(resolver.resolveNewVersion("1.2.3-pks-10", "master"), equalTo("1.2.3-master-1"));
+    }
+}
